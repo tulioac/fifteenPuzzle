5692a1860e717b8ad03fef9b412602b3012d52ef
tutex1500 <tutex1500@gmail.com>
2019-09-03 10:30:28 -0300

    Todo's e funções de movimento

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,37 +1,99 @@
 #include <iostream>
 
-#define tam 3
+// Tamanho da matriz[tam][tam]
+#define tam 4
 
 using namespace std;
 
-void up(int matriz[tam][tam], int &linha, int &coluna);
+// Esqueleto das funções
 
+// Funções de movimento
+// FIXME: Conferir se está nos limites
+void up(int matriz[tam][tam], int &linha, int &coluna);
+void down(int matriz[tam][tam], int &linha, int &coluna);
+void left(int matriz[tam][tam], int &linha, int &coluna);
+void right(int matriz[tam][tam], int &linha, int &coluna);
+
+// Função de exibição da matriz
 void exibeMatriz(int matriz[tam][tam]);
 
-int main()
-{
-    int matriz[tam][tam] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+// TODO: Conferir se está ordenada
+// TODO: Aumentar a dificuldade de acordo com o tamanho da matriz
+// TODO: Contador de movimentos
+// TODO: Cronômetro
 
-    int linha = 2;
-    int coluna = 2;
+// TODO: Allegro 5 - Eventos de teclado
+// TODO: Allegro 5 - Interface gráfica
 
-    cout << "Linha " << linha << " / Coluna: " << coluna << endl;
+int main() {
+    int matriz[tam][tam] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
 
+    // 01 02 03 04
+    // 05 06 07 08
+    // 09 10 11 12
+    // 13 14 15 00
+
+    int linha = 3;
+    int coluna = 3;
+
+    cout << "Inicial" << endl;
+    cout << "Linha " << linha << " / Coluna " << coluna << endl;
     exibeMatriz(matriz);
+
     up(matriz, linha, coluna);
+    cout << "Linha " << linha << " / Coluna " << coluna << endl;
     exibeMatriz(matriz);
 
-    cout << "Linha " << linha << " / Coluna: " << coluna << endl;
+    left(matriz, linha, coluna);
+    cout << "Linha " << linha << " / Coluna " << coluna << endl;
+    exibeMatriz(matriz);
+    
+    down(matriz, linha, coluna);
+    cout << "Linha " << linha << " / Coluna " << coluna << endl;
+    exibeMatriz(matriz);
+    
+    right(matriz, linha, coluna);
+    cout << "Linha " << linha << " / Coluna " << coluna << endl;
+    exibeMatriz(matriz);
 
     return 0;
 }
 
 void up(int matriz[tam][tam], int &linha, int &coluna) {
     // [i][j] => [i-1][j]
+    cout << "up()" << endl;
     int aux = matriz[linha][coluna];
     matriz[linha][coluna] = matriz[linha - 1][coluna];
     matriz[linha - 1][coluna] = aux;
     linha--;
+}
+
+void down(int matriz[tam][tam], int &linha, int &coluna) {
+    // [i][j] => [i+1][j]
+    cout << "down()" << endl;
+    int aux = matriz[linha][coluna];
+    matriz[linha][coluna] = matriz[linha + 1][coluna];
+    matriz[linha + 1][coluna] = aux;
+    linha++;
+}
+
+void left(int matriz[tam][tam], int &linha, int &coluna) {
+    // [i][j] => [i][j-1]
+    cout << "left()" << endl;
+    int aux = matriz[linha][coluna];
+    matriz[linha][coluna] = matriz[linha][coluna - 1];
+    matriz[linha][coluna - 1] = aux;
+    coluna--;
+    
+}
+
+void right(int matriz[tam][tam], int &linha, int &coluna) {
+    // [i][j] => [i][j+1]
+    cout << "right()" << endl;
+    int aux = matriz[linha][coluna];
+    matriz[linha][coluna] = matriz[linha][coluna + 1];
+    matriz[linha][coluna + 1] = aux;
+    coluna++;
 }
 
 void exibeMatriz(int matriz[tam][tam]) {
@@ -39,7 +101,8 @@ void exibeMatriz(int matriz[tam][tam]) {
     {
         for (int j = 0; j < tam; j++)
         {
-            cout << matriz[i][j] << " ";
+            printf("%.2d ", matriz[i][j]);
+            // cout << matriz[i][j] << " ";
         }
         cout << endl;
     }
