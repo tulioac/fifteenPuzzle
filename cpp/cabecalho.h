119c2a4db869db2076fd87164399b6ffd4755fa6
tutex1500 <tutex1500@gmail.com>
2019-09-03 11:08:55 -0300

    Remoção dos todos e fixme

--- cpp/cabecalho.h
+++ cpp/cabecalho.h
@@ -9,10 +9,7 @@
 
 using namespace std;
 
-// Esqueleto das funções
-
 // Funções de movimento
-// FIXME: Conferir se está nos limites
 void up(int [tam][tam], int &, int &);
 void down(int [tam][tam], int &, int &);
 void left(int [tam][tam], int &, int &);
@@ -20,15 +17,6 @@ void right(int [tam][tam], int &, int &);
 
 // Função de exibição da matriz
 void exibeMatriz(int [tam][tam]);
-
-// TODO: Conferir se está ordenada
-// TODO: Aumentar a dificuldade de acordo com o tamanho da matriz
-// TODO: Contador de movimentos
-// TODO: Cronômetro
-// TODO: Preencher a matriz com for
-
-// TODO: Allegro 5 - Eventos de teclado
-// TODO: Allegro 5 - Interface gráfica
 
 
 #endif
