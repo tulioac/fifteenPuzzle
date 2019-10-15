b2cb2ccd2e681d535fbb5a61b541901a73a052a2
tutex1500 <tutex1500@gmail.com>
2019-10-15 17:17:36 -0300

    Remoção de arquivo inútil

--- haskell/14ccf72b-bbf9-48d7-8b0c-922a264679fc.hs
+++
@@ -1,7 +0,0 @@
-exibeDificuldades =
-    putStrLn $ 
-    ("1 - Easy") ++ "\n" ++ 
-    ("2 - Medium") ++ "\n" ++
-    ("3 - Hard") ++ "\n" ++
-    ("4 - Very Hard") ++ "\n" ++
-    ("5 - Ultimate")
