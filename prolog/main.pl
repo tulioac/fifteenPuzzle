ca83a7d2637ecb571013980e34d6e4f44ac4231d
tutex1500 <tutex1500@gmail.com>
2019-11-24 11:06:03 -0300

    Verifica ordenação _com erro_

--- prolog/main.pl
+++ prolog/main.pl
@@ -1,40 +1,47 @@
-:- initialization main.
+%:- initialization main.
 
 bemVindo :- writeln("Seja bem vindo ao Fifteen Puzzle - Versão Prolog!").
 
 exibeDificuldades :- 
-        writeln("Selecione a dificuldade desejada:"),
-        writeln("1 - Fácil"),
-        writeln("2 - Médio"),
-        writeln("3 - Difícil"),
-        writeln("4 - Impossível").
+    writeln("Selecione a dificuldade desejada:"),
+    writeln("1 - Fácil"),
+    writeln("2 - Médio"),
+    writeln("3 - Difícil"),
+    writeln("4 - Impossível").
 
 solicitaDificuldade(R) :- 
-        read(R).
+    read(R).
 
 criaArrayOrdenado(Dificuldade, Lista) :-
-        ValorFinal is (Dificuldade * Dificuldade),
-        numlist(1, ValorFinal, Lista).
+    ValorFinal is (Dificuldade * Dificuldade),
+    numlist(1, ValorFinal, Lista).
 
 embaralhaArray(Array, ArrayEmbaralhado) :-
-        random_permutation(Array, ArrayEmbaralhado).
+    random_permutation(Array, ArrayEmbaralhado).
+
+verificaOrdenacao([], []).
+
+verificaOrdenacao([H1 | R1], [H2 | R2]) :-
+    H1 = H2,
+    verificaOrdenacao(R1, R2).
 
 main :- 
-        bemVindo,
-        exibeDificuldades,
-        solicitaDificuldade(Dificuldade),
-        writeln(Dificuldade),
-        criaArrayOrdenado(Dificuldade, Lista),
-        embaralhaArray(Lista, ListaEmbaralhada),
-        writeln(ListaEmbaralhada),
-        halt(0).
+    bemVindo,
+    exibeDificuldades,
+    solicitaDificuldade(Dificuldade),
+    writeln(Dificuldade),
+    criaArrayOrdenado(Dificuldade, ListaOrdenada),
+    embaralhaArray(ListaOrdenada, ListaEmbaralhada),
+    writeln(ListaEmbaralhada),
+    verificaOrdenacao(ListaOrdenada, ListaEmbaralhada),
+    halt(0).
 
 /* 
 
 DONE: Mensagem de bem vindo  
 
 DONE: Listar dificuldades
-DONE: Solicitar dificuldade     FIXME: Validar entrada
+DONE: Solicitar dificuldade         FIXME: Validar entrada
 
 DONE: Cria array de acordo com a dificuldade
 
@@ -48,7 +55,7 @@ HELP: Solicita entrada (setas ou wasd)
 
 Executa movimento de acordo com a entrada
 
-Verifica ordenação
+DONE: Verifica ordenação            FIXME: Quando é true não exib 
 
 EXTRA: Dicas de acordo com a quantidade de movimentos
 EXTRA: Porcentagem de resolução
