f18d34d765f0449146e7be01f2aec5d2abc8b4c7
tutex1500 <tutex1500@gmail.com>
2019-11-28 15:34:09 -0300

    Move Esquerda e Exibe na tela

--- prolog/lista.pl
+++ prolog/lista.pl
@@ -13,8 +13,25 @@ indiceDoElemento([_|Cauda], Elemento, Indice) :-
     !,
     Indice is Indice1 + 1.
 
+/*Valor is (Dificuldade + 2) ** 2,*/
 pegaIndiceEspacoVazio(Lista, Dificuldade, Indice) :-
-    Valor is (Dificuldade + 2) ** 2,
-    indiceDoElemento(Lista, Valor, Indice).
+    indiceDoElemento(Lista, Dificuldade, Indice).
 
+moveEsquerda(Lista, Tamanho, R) :-
+    ValorMaximo is Tamanho ** 2,
+    pegaIndiceEspacoVazio(Lista, ValorMaximo, IndiceEspacoVazio),
+    writeln(IndiceEspacoVazio),
+    Modulo is (IndiceEspacoVazio mod Tamanho),
+    Modulo \= 0,
+    writeln(Modulo),
+    IndiceMenosUm is IndiceEspacoVazio - 1,
+    trocaElementos(Lista, IndiceEspacoVazio, IndiceMenosUm, R).
 
+exibeNaTela([], NumElementosArray) :- NumElementosArray =:= 0.
+exibeNaTela([H|T], TamanhoLinha, NumElementosArray):-
+    ((NumElementosArray-1) mod (TamanhoLinha)) =:= 0 ->
+        write(H), nl, X2 is NumElementosArray-1, exibeNaTela(T, TamanhoLinha, X2);
+    write(H),
+    write(" "),
+    X2 is NumElementosArray-1,
+    exibeNaTela(T, TamanhoLinha,X2).
