48f81fe2f9126418b0868be61b44c0161aeaf89b
tutex1500 <tutex1500@gmail.com>
2019-11-28 15:41:19 -0300

    Move Direita

--- prolog/lista.pl
+++ prolog/lista.pl
@@ -27,8 +27,19 @@ moveEsquerda(Lista, Tamanho, R) :-
     IndiceMenosUm is IndiceEspacoVazio - 1,
     trocaElementos(Lista, IndiceEspacoVazio, IndiceMenosUm, R).
 
+moveDireita(Lista, Tamanho, R) :-
+    ValorMaximo is Tamanho ** 2,
+    pegaIndiceEspacoVazio(Lista, ValorMaximo, IndiceEspacoVazio),
+    writeln(IndiceEspacoVazio),
+    Modulo is (IndiceEspacoVazio mod Tamanho),
+    Condicao is Tamanho - 1,
+    Modulo \= Condicao,
+    writeln(Modulo),
+    IndiceMaisUm is IndiceEspacoVazio + 1,
+    trocaElementos(Lista, IndiceEspacoVazio, IndiceMaisUm, R).
+
 exibeNaTela([], NumElementosArray) :- NumElementosArray =:= 0.
-exibeNaTela([H|T], TamanhoLinha, NumElementosArray):-
+exibeNaTela([H|T], TamanhoLinha, NumElementosArray) :-
     ((NumElementosArray-1) mod (TamanhoLinha)) =:= 0 ->
         write(H), nl, X2 is NumElementosArray-1, exibeNaTela(T, TamanhoLinha, X2);
     write(H),
