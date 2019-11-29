4499536bc32548954088be8a9f655d19d21c57f5
tutex1500 <tutex1500@gmail.com>
2019-11-28 21:56:17 -0300

    Exibe matriz de guilherme

--- prolog/lista.pl
+++ prolog/lista.pl
@@ -48,16 +48,6 @@ moveBaixo(Lista, Tamanho, R) :-
     Posicao is IndiceEspacoVazio + Tamanho,
     trocaElementos(Lista, IndiceEspacoVazio, Posicao, R).
 
-exibeNaTela([], NumElementosArray) :- NumElementosArray =:= 0.
-exibeNaTela([H|T], TamanhoLinha, NumElementosArray) :-
-    ((NumElementosArray-1) mod (TamanhoLinha)) =:= 0 ->
-        write(H), nl, X2 is NumElementosArray-1, exibeNaTela(T, TamanhoLinha, X2);
-    write(H),
-    write(' '),
-    X2 is NumElementosArray-1,
-    exibeNaTela(T, TamanhoLinha,X2).
-
-
 solicitaMovimento(R) :-
     writeln('Digite o movimento desejado w / a / s / d'),
     read(R).
@@ -76,3 +66,50 @@ validaMovimento(R) :-
     writeln('Movimento invalido, tente novamente.'),
     solicitaMovimento(R).
  */
+
+
+exibeNaTela([], NumElementosArray) :- NumElementosArray =:= 0.
+exibeNaTela([H|T], TamanhoLinha, NumElementosArray) :-
+    ((NumElementosArray-1) mod (TamanhoLinha)) =:= 0 ->
+        write(H), nl, X2 is NumElementosArray-1, exibeNaTela(T, TamanhoLinha, X2);
+    write(H),
+    write(" "),
+    X2 is NumElementosArray-1,
+    exibeNaTela(T, TamanhoLinha,X2).
+    
+    
+    /*Exibe Lista na tela recebendo como parâmetro o tamanho da linha e o tamanho da lista*/
+exibeNaTela([], TamanhoLinha, NumElementosArray) :- NumElementosArray =:= 0.
+exibeNaTela([H|T], TamanhoLinha, NumElementosArray):-
+    (H =:= TamanhoLinha**2 ->
+        write("X"), write(" "), X2 is NumElementosArray-1, exibeNaTela(T, TamanhoLinha, X2));
+    ((NumElementosArray-1) mod (TamanhoLinha)) =:= 0 -> 
+        (H < 10 -> write("0")),
+        writeln(H), X2 is NumElementosArray-1, exibeNaTela(T, TamanhoLinha, X2);
+
+    (H < 10 -> write("0"),write(H),
+        write(" "),
+        X2 is NumElementosArray-1,
+        exibeNaTela(T, TamanhoLinha,X2));
+
+    write(H),
+    write(" "),
+    X2 is NumElementosArray-1,
+    exibeNaTela(T, TamanhoLinha,X2).
+
+/*Encapsula exibição de Lista na tela*/
+exibeArray([]).
+exibeArray(A):- 
+    length(A, TamanhoLista),
+    exibeNaTela(A,TamanhoLista/integer(sqrt(TamanhoLista)),
+    TamanhoLista).
+
+/*Acha elemento pelo indice*/
+
+indiceDoElemento([Elemento|_], Elemento, 0) :- !.
+indiceDoElemento([_|Cauda], Elemento, Indice) :-
+    indiceDoElemento(Cauda, Elemento, Indice1),
+    !,
+    Indice is Indice1 + 1.
+
+/*Apagar valor específico*/
